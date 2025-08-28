211208c2b01e880a25fec9f6390668044173028d
Shawn Page <shawn.page@gmail.com>
2025-08-28 09:44:13 -0400

    Added RequestMapping annotations. Added a Customer model, service and controller. Specify the lombok version.

---
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -0,0 +1,61 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Slf4j
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private Map<UUID, Customer> customerMap;
+
+    public CustomerServiceImpl() {
+        this.customerMap = new HashMap<>();
+
+        Customer customer1 = Customer.builder()
+                .id(UUID.randomUUID())
+                .name("John Doe")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer2 = Customer.builder()
+                .id(UUID.randomUUID())
+                .version(2)
+                .name("Jane Doe")
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer3 = Customer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .name("Dom Deluis")
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        customerMap.put(customer1.getId(), customer1);
+        customerMap.put(customer2.getId(), customer2);
+        customerMap.put(customer3.getId(), customer3);
+    }
+
+    @Override
+    public List<Customer> listCustomers() {
+        return new ArrayList<>(customerMap.values());
+    }
+
+    @Override
+    public Customer getCustomerById(UUID id) {
+
+        log.debug("Get customer by Id - in service. Id: " +  id.toString());
+
+        return customerMap.get(id);
+
+    }
+}
